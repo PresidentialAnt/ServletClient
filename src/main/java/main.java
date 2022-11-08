33bedac4a4e6e625bb6dcf7a08b70dd5314d2b89
PresidentialAnt <antonio.waiming@gmail.com>
2022-11-08 17:18:05 +0000

    V1

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class main {
+import java.io.*;
+import java.net.*;
+
+public class main {
+    public static void main(String[] args) {
+        URL myURL = null;
+        try {
+            myURL = new URL("https://imperial.ac.uk");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        HttpURLConnection conn = null;
+        try {
+            conn = (HttpURLConnection) myURL.openConnection();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            conn.setRequestMethod("GET");
+        } catch (ProtocolException e) {
+            throw new RuntimeException(e);
+        }
+        conn.setRequestProperty("Accept", "text/html");
+        conn.setRequestProperty("charset", "utf-8");
+
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(
+                    new InputStreamReader(myURL.openStream()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String inputLine;
+        while (true) {
+            try {
+                if (!((inputLine = in.readLine()) != null)) break;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(inputLine);
+        }
+        try {
+            in.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
